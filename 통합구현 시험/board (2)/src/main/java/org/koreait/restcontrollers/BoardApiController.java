698f43f70e1903c74fad63459b916eab4039661d
Jongpyo Hong <djurev@naver.com>
2023-06-08 00:01:44 +0900

    통합구현 시험 연습

--- 통합구현 시험/board (2)/src/main/java/org/koreait/restcontrollers/BoardApiController.java
+++ 통합구현 시험/board (2)/src/main/java/org/koreait/restcontrollers/BoardApiController.java
@@ -3,9 +3,15 @@ package org.koreait.restcontrollers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.koreait.controllers.BoardForm;
+import org.koreait.entities.BoardData;
+import org.koreait.models.board.BoardDeleteService;
+import org.koreait.models.board.BoardInfoService;
+import org.koreait.models.board.BoardListService;
 import org.koreait.models.board.BoardSaveService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Log
@@ -15,6 +21,10 @@ import org.springframework.web.bind.annotation.*;
 public class BoardApiController {
 
     private final BoardSaveService saveService;
+    private final BoardInfoService infoService;
+    private final BoardListService listService;
+    private final BoardDeleteService deleteService;
+
 
     @PostMapping("/write")
     public ResponseEntity<Object> write(@RequestBody BoardForm boardForm) { // 요청을 json 형식으로
@@ -24,4 +34,5 @@ public class BoardApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED).build(); // CREATED : 201
     }
+
 }
